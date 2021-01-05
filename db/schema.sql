a8a42845e5f37e41dbb4e513cb5053bf2d14fa94
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-01-05 14:40:19 -0600

    successful db schema queries to initilize application

--- db/schema.sql
+++ db/schema.sql
@@ -2,27 +2,25 @@ DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS employees;
 
---*is there a standard order to create the db tables? it appears that we should be creating smaller tables first OR we should be creating the tables that do not have an foreign keys... 
-
 CREATE TABLE IF NOT EXISTS departments(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    department_id INT AUTO_INCREMENT PRIMARY KEY,
     department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS roles(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    roles_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
-    -- the DECIMAL data type references the number of digits and how many can be a decimal place
     salary DECIMAL(8,2),
-    --! department_id needs to be a foreign key to the dpeartments table
-    department_id INT NOT NULL
+    department_id INT NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(department_id)
 );
 
 CREATE TABLE IF NOT EXISTS employees(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    emp_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    --! role_id and manager_id need to be foreign keys to the roles table   
-    role_id INT NOT NULL,
-    manager_id INT NOT NULL
+    roles_id INT NOT NULL,
+    manager_id INT,
+    CONSTRAINT fk_role FOREIGN KEY (roles_id) REFERENCES roles(roles_id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(emp_id)
 );
