ffe55a9460b792b6b417323e158e0d541cfcaf93
minnesotaNyst <71350960+minnesotaNyst@users.noreply.github.com>
2021-01-05 20:20:30 -0600

    working on getting the server.js file to return query results

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
-const mysql = require("mysql2");
-const inquirer = require("inquirer");
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
 const cTable = require('console.table');
 
 // create the connection to database
 const connection = mysql.createConnection({
-    host: 'localhost',
-    // port: 3005,
-    user: 'user',
-    // password: 'dolphinsarecool',
-    database: 'employee_tracker'
-  });
+	host: 'localhost',
+	// port: 3005,
+	user: 'user',
+	// password: 'dolphinsarecool',
+	database: 'employee_tracker'
+});
 
-  connection.query(
-      
-  )
+// simple query
+connection.query(
+	'SELECT * FROM roles WHERE roles_id = 1',
+	function (err, res, fields) {
+		console.table(res); // results contains rows returned by server
+		console.log(fields); // fields contains extra meta data about results, if available
+	}
+);
